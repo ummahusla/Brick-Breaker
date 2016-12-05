8897df1a20246bb8b20afcd585bc847d73806f94
Edvins Antonovs <ummahusla@gmail.com>
2016-12-05 21:59:22 +0000

    :fire: Added paddle movement

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+        // Initial paddle position
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+        // Get the X position from mouse input
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        // Set minimum and max width
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+        // Move paddle depends on position
+        this.transform.position = paddlePos; 
 	}
 }
